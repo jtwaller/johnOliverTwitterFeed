71127dd59c13b8a121509d599526bc3a3c6ed8d8
Jack Waller <ucdwaller@gmail.com>
2018-05-02 14:46:39 -0700

    Temp html / css implementation
    Implementation mostly taken from below:
    https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var Twitter = require('twitter');
 
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
